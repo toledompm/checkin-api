e7c729bcc0fd7332047af74d9633f76168f98df0
toledompm <toledompm@gmail.com>
2021-06-06 00:41:05 -0300

    fix: provider dependencies

--- src/checkin/checkin.module.ts
+++ src/checkin/checkin.module.ts
@@ -2,15 +2,16 @@ import { Module } from '@nestjs/common';
 import { CheckinController } from 'src/checkin/checkin.controller';
 import { CheckinServiceImpl } from 'src/checkin/checkin.service.impl';
 import { CHECKIN_SERVICE } from 'src/checkin/checkin.constants';
+import { UserModule } from 'src/user/user.module';
 
 const checkinServiceProvider = {
   provide: CHECKIN_SERVICE,
-  useValue: new CheckinServiceImpl(),
+  useClass: CheckinServiceImpl,
 };
 
 @Module({
-  imports: [],
-  controllers: [CheckinController],
+  imports: [UserModule],
   providers: [checkinServiceProvider],
+  controllers: [CheckinController],
 })
 export class CheckinModule {}
