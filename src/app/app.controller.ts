f976607c19aedeb7dde6c382a3deae580a6aafe5
toledompm <toledompm@gmail.com>
2021-06-06 14:46:24 -0300

    feat: use guards

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from 'src/app/app.service';
+import { JWT_AUTH_STRATEGY } from 'src/auth/auth.constants';
 
 @Controller()
+@UseGuards(AuthGuard(JWT_AUTH_STRATEGY))
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
